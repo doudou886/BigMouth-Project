c11f8893fa813390718deca26dd17fcda493f129
renwei1029 <45345155+renwei1029@users.noreply.github.com>
2023-11-07 16:26:51 +0800

    white list

--- BigMouth.sol
+++ BigMouth.sol
@@ -14,6 +14,7 @@ contract BigMouth is ERC721Enumerable, Ownable {
 
     // Constants
     uint256 public constant MAX_SUPPLY = 1000;
+    
     uint256 public mintPrice = 0.1 ether;
     uint256 public maxBalance = 1;
     uint256 public maxMint = 1;
@@ -24,6 +25,12 @@ contract BigMouth is ERC721Enumerable, Ownable {
 
     mapping(uint256 => string) private _tokenURIs;
 
+    // Whitelist
+    mapping(address => bool) public whitelist;
+    uint256 public whitelistMaxSupply = 100;
+    bool public _isWhitelistSaleActive = false;
+    uint256 public mintPriceWhitelist = 0.01 ether;
+
     constructor(string memory initBaseURI, string memory initNotRevealedUri)
         ERC721("Big Mouth", "BM")
     {
@@ -31,6 +38,55 @@ contract BigMouth is ERC721Enumerable, Ownable {
         setNotRevealedURI(initNotRevealedUri);
     }
 
+    // 白名单铸造
+    function whitelistMint(uint256 tokenQuantity) public payable {
+        // 白名单相关检查
+        require(whitelist[msg.sender], "Not in whitelist");
+        require(_isWhitelistSaleActive, "Whitelist Sale must be active to mint BigMouths");
+        require(
+            totalSupply() + tokenQuantity <= whitelistMaxSupply,
+            "Sale would exceed whitelist max supply"
+        );
+        
+        // 通用检查
+        require(
+            totalSupply() + tokenQuantity <= MAX_SUPPLY,
+            "Sale would exceed max supply"
+        );
+        require(
+            balanceOf(msg.sender) + tokenQuantity <= maxBalance,
+            "Sale would exceed max balance"
+        );
+        require(
+            tokenQuantity * mintPriceWhitelist <= msg.value,
+            "Not enough ether sent"
+        );
+        require(tokenQuantity <= maxMint, "Can only mint 1 tokens at a time");
+
+        _mintBigMouth(tokenQuantity);
+    }
+
+    // 批量新增 白名单地址
+    function batchAddToWhitelist(address[] memory _newEntry) external onlyOwner {
+        // 每次最多批量添加200个白名单，参数是一个地址的数组 ["0x...","0x...","0x..."]
+        require(_newEntry.length <= 200,"to is more than 200");
+        for (uint8 i = 0; i < _newEntry.length; i++) {
+			whitelist[_newEntry[i]] = true;
+		}
+    }
+
+    // 移除 白名单地址
+    function removeFromWhitelist(address _newEntry) external onlyOwner {
+        require(whitelist[_newEntry], "Previous not in whitelist");
+        whitelist[_newEntry] = false;
+    }
+
+    // 检查某个地址是否在白名单中，true在，false不在
+    function isAddressWhitelisted(address _address) public view returns (bool) {
+        return whitelist[_address];
+    }
+
+    // 普通铸造
     function mintBigMouth(uint256 tokenQuantity) public payable {
         require(
             totalSupply() + tokenQuantity <= MAX_SUPPLY,
@@ -97,6 +153,10 @@ contract BigMouth is ERC721Enumerable, Ownable {
     }
 
     //only owner
+    function flipWhitelistSaleActive() public onlyOwner {
+        _isWhitelistSaleActive = !_isWhitelistSaleActive;
+    }
+
     function flipSaleActive() public onlyOwner {
         _isSaleActive = !_isSaleActive;
     }
@@ -130,6 +190,10 @@ contract BigMouth is ERC721Enumerable, Ownable {
 
     function setMaxMint(uint256 _maxMint) public onlyOwner {
         maxMint = _maxMint;
+    }
+
+    function setWhitelistMaxSupply(uint256 _whitelistMaxSupply) public onlyOwner {
+        whitelistMaxSupply = _whitelistMaxSupply;
     }
 
     function withdraw(address to) public onlyOwner {
